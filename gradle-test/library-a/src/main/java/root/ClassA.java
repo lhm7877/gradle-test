938813af199972b91ea6abfa935d5063386c66d3
이후민 <lhm7877>
2018-09-17 16:39:10 +0900

    classA, classB추가

---
+++ gradle-test/library-a/src/main/java/root/ClassA.java
@@ -0,0 +1,11 @@
+package root;
+
+/**
+ * @author : lhm0805
+ * @date : 2018-09-17
+ **/
+public class ClassA {
+    public void sayHi(){
+        System.out.println(this.toString() + " HI!");
+    }
+}
---
+++ gradle-test/library-b/src/main/java/root/ClassB.java
@@ -0,0 +1,8 @@
+package root;
+
+/**
+ * @author : lhm0805
+ * @date : 2018-09-17
+ **/
+public class ClassB {
+}
